28ed7550d81d350de012d7d5630ff028a50e084c
Paurnima <rajputp2007@gmail.com>
2022-01-21 18:30:42 +0530

    UI manage

--- src/app/create-resume/viewer/viewer.component.ts
+++ src/app/create-resume/viewer/viewer.component.ts
@@ -12,7 +12,39 @@ import html2canvas from 'html2canvas';
 })
 export class ViewerComponent  {
   data;
+  
+
   @ViewChild('pdfElement') pdfElement: ElementRef;
   constructor(private resumeService: ResumeServiceService ) { }
+
+  ngOnInit(): void {
+    this.getResumeDataFromService();
+    this.getPrintCommand();
+  }
+
+  getResumeDataFromService(): any{
+   this.resumeService.getResumeData().subscribe((v) => {
+     console.log(v);
+     this.data = v;
+   });
+  }
+
+  getPrintCommand(): any{
+    this.resumeService.getPrintCommand().subscribe( (event) => {
+      html2canvas(this.pdfElement.nativeElement).then(canvas => {
+        const img = canvas.toDataURL('image/png');
+        const pdf = new jsPDF({
+          orientation: 'portrait',
+          format: 'a4'
+        });
+        const imgProps = pdf.getImageProperties(img);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(img, 'JPEG', 0, 0, 0, 0, null, 'FAST', 0);
+        pdf.save('test.pdf');
+      });
+    });
+  }
+
 
 }
